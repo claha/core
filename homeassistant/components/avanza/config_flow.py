5e4348af15c5a5759ec2e0c42b047402226d9c9b
Claes Hallstrom <hallstrom.claes@gmail.com>
2020-09-30 20:31:27 +0200

    Initial config flow

--- homeassistant/components/avanza/config_flow.py
+++ homeassistant/components/avanza/config_flow.py
@@ -1,58 +1,42 @@
 """Config flow for Avanza integration."""
 import logging
 
+import pyavanza
 import voluptuous as vol
 
-from homeassistant import config_entries, core, exceptions
+from homeassistant import config_entries
+from homeassistant.helpers import aiohttp_client
 
 from .const import DOMAIN  # pylint:disable=unused-import
 
 _LOGGER = logging.getLogger(__name__)
 
-# TODO adjust the data schema to the data that you need
-DATA_SCHEMA = vol.Schema({"host": str, "username": str, "password": str})
-
-
-class PlaceholderHub:
-    """Placeholder class to make tests pass.
-
-    TODO Remove this placeholder class and replace with things from your PyPI package.
-    """
-
-    def __init__(self, host):
-        """Initialize."""
-        self.host = host
-
-    async def authenticate(self, username, password) -> bool:
-        """Test if we can authenticate with the host."""
-        return True
-
-
-async def validate_input(hass: core.HomeAssistant, data):
-    """Validate the user input allows us to connect.
-
-    Data has the keys from DATA_SCHEMA with values provided by the user.
-    """
-    # TODO validate the data can be used to set up a connection.
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
-
-    hub = PlaceholderHub(data["host"])
-
-    if not await hub.authenticate(data["username"], data["password"]):
-        raise InvalidAuth
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
-    # Return info that you want to store in the config entry.
-    return {"title": "Name of the device"}
+INSTRUMENT_SEARCH_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(
+            "instrument_type", default=pyavanza.InstrumentType.ANY.value
+        ): vol.In(
+            [
+                pyavanza.InstrumentType.ANY.value,
+                pyavanza.InstrumentType.BOND.value,
+                pyavanza.InstrumentType.CERTIFICATE.value,
+                pyavanza.InstrumentType.CONVERTIBLE.value,
+                pyavanza.InstrumentType.EQUITY_LINKED_BOND.value,
+                pyavanza.InstrumentType.EXCHANGE_TRADED_FUND.value,
+                pyavanza.InstrumentType.FUND.value,
+                pyavanza.InstrumentType.FUTURE_FORWARD.value,
+                pyavanza.InstrumentType.INDEX.value,
+                pyavanza.InstrumentType.OPTION.value,
+                pyavanza.InstrumentType.PREMIUM_BOND.value,
+                pyavanza.InstrumentType.RIGHT.value,
+                pyavanza.InstrumentType.STOCK.value,
+                pyavanza.InstrumentType.SUBSCRIPTION_OPTION.value,
+                pyavanza.InstrumentType.WARRANT.value,
+            ]
+        ),
+        vol.Required("instrument_query"): str,
+    }
+)
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -63,28 +47,54 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
+        return await self.async_step_instrument_search()
+
+    async def async_step_instrument_search(self, user_input=None):
+        """Handle the search for an instrument."""
         errors = {}
         if user_input is not None:
             try:
-                info = await validate_input(self.hass, user_input)
-
-                return self.async_create_entry(title=info["title"], data=user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidAuth:
-                errors["base"] = "invalid_auth"
+                session = aiohttp_client.async_get_clientsession(self.hass)
+                query = user_input["instrument_query"]
+                instrument = pyavanza.InstrumentType[user_input["instrument_type"]]
+                instruments = await pyavanza.search_async(
+                    session,
+                    query,
+                    limit=10,
+                    instrument=instrument,
+                )
+                return await self.async_step_instrument_select(user_input=instruments)
+            except pyavanza.AvanzaResponseError:
+                errors["base"] = "response_error"
+            except pyavanza.AvanzaRequestError:
+                errors["base"] = "request_error"
+            except pyavanza.AvanzaParseError:
+                errors["base"] = "parse_error"
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
 
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+            step_id="instrument_search",
+            data_schema=INSTRUMENT_SEARCH_DATA_SCHEMA,
+            errors=errors,
         )
 
-
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
-
-class InvalidAuth(exceptions.HomeAssistantError):
-    """Error to indicate there is invalid auth."""
+    async def async_step_instrument_select(self, user_input):
+        """Handle the selection of an instrument."""
+        errors = {}
+        if isinstance(user_input, list):
+            instruments = user_input
+            data_schema = vol.Schema(
+                {
+                    vol.Required("instrument"): vol.In(
+                        [instrument.name for instrument in instruments]
+                    )
+                }
+            )
+            return self.async_show_form(
+                step_id="instrument_select", data_schema=data_schema, errors=errors
+            )
+        else:
+            instrument = user_input
+            return self.async_create_entry(title=instrument, data={})
